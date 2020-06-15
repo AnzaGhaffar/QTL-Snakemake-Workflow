752efbf5d357d0d9b267a93229ae24ec64f74da3
AnzaGhaffar <anza.ghaffar@hotmail.com>
2020-06-15 11:18:39 +0200

    resolving the documentation error

--- source/conf.py
+++ source/conf.py
@@ -23,7 +23,8 @@ author = 'Anza Ghaffar'
 
 # The full version, including alpha/beta/rc tags
 release = 'V1'
-
+# added by anza for resolving the error of content.rst not found
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
